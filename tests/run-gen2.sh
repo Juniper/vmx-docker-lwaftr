34410457e85cdf3c9e087e561ad3de351bb02be0
Marcel Wiget <mwiget@juniper.net>
2016-09-09 15:33:12 +0200

    match run-gen2.sh settings to lwaftr2.txt

--- tests/run-gen2.sh
+++ tests/run-gen2.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-cp ../build/snabb .
+SNABB=../build/snabb
 
 PCIDEV=0000:05:00.1
 INTERFACE=p1p2
-RATE=3.1
+RATE=0.1
 CPU=7
 if [ -z "$RATE" ]; then
   echo "Please specify rate in MPPS"
@@ -15,7 +15,7 @@ SMAC="02:02:02:02:02:02"
 DMAC="02:42:df:27:05:00"
 echo $INTERFACE at $PCIDEV has $MAC, running at $RATE gbps
 
-CMD="sudo taskset -c $CPU ./snabb packetblaster lwaftr $OPT  --src_mac $SMAC --dst_mac $DMAC --b4 2a02:587:f710::40,10.10.0.0,1024 --aftr 2a02:587:f700::100 --count $FLOWS --rate $RATE --pci $PCIDEV # size --128  #--vlan 5"
+CMD="sudo taskset -c $CPU $SNABB packetblaster lwaftr $OPT  --src_mac $SMAC --dst_mac $DMAC --b4 2001:db8::40,10.10.0.0,1024 --aftr 2001:db8:ffff::100 --count $FLOWS --rate $RATE --pci $PCIDEV # size --128  #--vlan 5"
 
 echo $CMD
 
