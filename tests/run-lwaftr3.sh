37af2667f3931e84bb9d3c6428e4cdb04f6c20f4
Marcel Wiget <mwiget@juniper.net>
2016-09-18 14:53:26 +0200

    adding 200k softwire binding config

---
+++ tests/run-lwaftr3.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+NAME="lwaftr3"
+CFG="lwaftr3.txt"
+#VMX="vmx-bundle-16.1R1.7.tgz"
+VMX="vmx-bundle-16.1-20160915.0.tgz"
+CONTAINER="$(cat ../VERSION)"
+IDENTITY="snabbvmx.key"
+chmod 400 $IDENTITY
+INTERFACES="0000:05:00.0/6"
+LICENSE="license-eval.txt"
+
+docker rm $NAME 2>/dev/null
+docker run --name $NAME -ti --privileged -v $PWD:/u:ro $CONTAINER -I $IDENTITY -l $LICENSE -c $CFG $VMX $INTERFACES
+docker rm $NAME
