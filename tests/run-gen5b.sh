6b080a9da02ed6d09d6aa38d0143b6b6be242015
Marcel Wiget <mwiget@juniper.net>
2016-10-17 12:49:01 +0200

    lwaftr5 test case to use vlan tag on 0/0/1

--- tests/run-gen5b.sh
+++ tests/run-gen5b.sh
@@ -3,7 +3,7 @@
 SNABB=../snabb/src/snabb
 
 PCIDEV=0000:04:00.1
-INTERFACE=enp4s0f0
+INTERFACE=ens6f1
 RATE=3.1
 CPU=4
 if [ -z "$RATE" ]; then
@@ -16,7 +16,8 @@ DMAC="02:b0:53:89:04:00"
 
 echo $INTERFACE at $PCIDEV has $MAC, running at $RATE gbps
 
-CMD="sudo taskset -c $CPU $SNABB packetblaster lwaftr $OPT  --src_mac $SMAC --dst_mac $DMAC --b4 2001:db8::40,10.10.0.0,1024 --aftr 2001:db8:ffff::100 --count $FLOWS --rate $RATE --pci $PCIDEV # size --128  #--vlan 5"
+#CMD="sudo taskset -c $CPU $SNABB packetblaster lwaftr $OPT  --src_mac $SMAC --dst_mac $DMAC --b4 2001:db8::40,10.10.0.0,1024 --aftr 2001:db8:ffff::100 --count $FLOWS --rate $RATE --pci $PCIDEV"
+CMD="sudo taskset -c $CPU $SNABB packetblaster lwaftr $OPT  --src_mac $SMAC --dst_mac $DMAC --b4 2001:db8::40,10.10.0.0,1024 --aftr 2001:db8:ffff::100 --count $FLOWS --rate $RATE --pci $PCIDEV --vlan 100"
 
 echo $CMD
 
