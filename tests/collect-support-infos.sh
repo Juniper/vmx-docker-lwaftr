8689ddabdd1085273c3513218ea50ac5051f6094
Marcel Wiget <mwiget@juniper.net>
2016-11-08 14:33:48 +0100

    remove stats.xml from collector. No longer exists

--- tests/collect-support-infos.sh
+++ tests/collect-support-infos.sh
@@ -6,7 +6,7 @@ if [ -z "$CONTAINER" ]; then
   exit 1
 fi
 echo "collecting data in container $CONTAINER ..."
-docker exec -ti $CONTAINER bash -c "cd /tmp; (ps axw && lscpu && cat /proc/meminfo && /show_affinity.sh) > sysinfo.txt; tar zcf /tmp/support-info.tgz /VERSION *.log mac_* pci_* *txt *txt.s *.cfg *.conf stats.xml config.* test*.sh ../config_drive/vmm-config.tgz ../root/.bashrc /usr/local/bin/snabb" 
+docker exec -ti $CONTAINER bash -c "cd /tmp; (ps axw && lscpu && cat /proc/meminfo && /show_affinity.sh) > sysinfo.txt; tar zcf /tmp/support-info.tgz /VERSION *.log mac_* pci_* *txt *txt.s *.cfg *.conf config.* test*.sh ../config_drive/vmm-config.tgz ../root/.bashrc /usr/local/bin/snabb" 
 echo "transferring data from the container to host ..."
 docker cp $CONTAINER:/tmp/support-info.tgz support-info-$TIMESTAMP.tgz
 ls -l support-info-$TIMESTAMP.tgz
