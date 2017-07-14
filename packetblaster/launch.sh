56284c668d83762098392efac7c685b3af5a9b7d
Marcel Wiget <mwiget@gmail.com>
2017-07-14 03:24:35 +0200

    wait until lwaftr is reachable

--- packetblaster/launch.sh
+++ packetblaster/launch.sh
@@ -1,7 +1,16 @@
 #!/bin/ash
-ping -c 3 lwaftr
+
+while :
+do
+  ping -c 3 lwaftr
+  if [ 0 == $? ]; then
+    break;
+  fi
+  echo "waiting for lwaftr to be reachable ..."
+  sleep 5
+done
+
 dstmac=$(arp -na | awk '{print $4}')
 srcmac=$(ifconfig eth0|grep HWaddr|awk '{print $5}')
-
 echo "srcmac=$srcmac -> dstmac=$dstmac"
 /usr/bin/snabb packetblaster lwaftr --src_mac $srcmac --dst_mac $dstmac --int eth0 $@
